8d4d48cb4cbd257fdf04653c0ee7180b3a39913c
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2025-02-06 10:35:49 +0700

    [Edited] - Updated color of the year

--- src/components/AfterInstallation.jsx
+++ src/components/AfterInstallation.jsx
@@ -2,24 +2,24 @@ const AfterInstallation = () => {
   return (
     <div
       data-testid="after-installation-container"
-      className="w-full bg-linear-to-l from-[#fff] to-[#FFBE98] p-1.5"
+      className="w-full bg-linear-to-l from-[#fff] to-[#9E7A68]/40 p-1.5"
     >
       <div className="bg-base-100 relative p-8">
         <sup className="absolute top-10 right-6 flex items-center gap-2 font-bold">
-          <div className="h-2 w-2 bg-[#FFBE98]" />
-          Pantone color of the year 2023 - Viva Magenta
+          <div className="h-2 w-2 bg-[#9E7A68]" />
+          Pantone Color of the Year 2025 - Mocca Mousse
         </sup>
-        <div className="bg-linear-to-l from-[#964F4C] to-[#964F4C] bg-clip-text text-h3 font-bold text-transparent">
+        <div className="from-pr text-h3 bg-linear-to-l from-[#964F4C] to-[#964F4C] bg-clip-text font-bold text-transparent">
           After Installation
         </div>
         <div className="py-2">
           <strong>
             You have to edit text by name below this{" "}
-            <span className="text-[#964F4C]">
+            <span className="text-primary">
               {'(Trick: use "Find All and Replace")'}
             </span>
           </strong>
-          <ul className="list-[upper-roman] pl-8 pt-4">
+          <ul className="list-[upper-roman] pt-4 pl-8">
             <li>REPLACE_WITH_YOUR_PROJECT_NAME</li>
             <li>REPLACE_WITH_YOUR_PROJECT_DESCRIPTION</li>
             <li>REPLACE_WITH_YOUR_APP_API</li>
@@ -29,7 +29,7 @@ const AfterInstallation = () => {
             <li>REPLACE_WITH_YOUR_DOCKER_REGISTRY</li>
           </ul>
         </div>
-        <div className="pt-4 text-small leading-tight text-[#964F4C]">
+        <div className="text-small pt-4 leading-tight text-[#964F4C]">
           Note: REPLACE_WITH_YOUR_SONARQUBE_IP_AND_PORT should be like
           {'"sub.host.com:9000" or "172.1.2.3:9000"'}
         </div>
